1fc24212e2266b0730b76aba06cfa67e1781904f
Quique Fdez Guerra <quique@ckgrafico.com>
2019-03-10 16:31:25 +0100

    Add react example with hooks

---
+++ examples/react-example/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
