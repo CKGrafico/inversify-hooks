b8bceb8834f41f8fa8ff6f2ea6b143480d74dbce
Quique Fdez Guerra <efernandez@plainconcepts.com>
2019-10-25 09:02:47 +0200

    Start new approach

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-export * from 'inversify-props';
+import { cid, Container as PropsContainer, Inject, inject, injectable } from 'inversify-props';
 import { useContainer } from './use-service.hook';
 
-export {
-  useContainer
-};
+class Container extends PropsContainer {
+
+}
+
+const container: Container = new Container();
+
+export { useContainer, Inject, inject, injectable, Container, cid };
+
