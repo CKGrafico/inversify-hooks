dea6461a74f2cef3ef30b1c314e773935c83899d
Quique Fdez Guerra <efernandez@plainconcepts.com>
2019-10-25 09:05:16 +0200

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
+import { interfaces } from 'inversify';
 import { cid, Container as PropsContainer, Inject, inject, injectable } from 'inversify-props';
 import { useContainer } from './use-service.hook';
 
 class Container extends PropsContainer {
+  public addSingleton<T>(constructor: {
+    new(...args: any[]): T;
+  }, id?: string): interfaces.BindingWhenOnSyntax<T> {
 
+    const binding = super.addSingleton<T>(constructor);
+
+    return binding;
+  }
 }
 
 const container: Container = new Container();
